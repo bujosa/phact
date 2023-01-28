ab0e64056fca3a3036aa2f4e16e0beb9d16375b4
bujosa <davidbujosa@gmail.com>
2023-01-28 04:12:55 -0400

    feat: add counter example

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: '0.8.17',
+  networks: {
+    goerli: {
+      url: process.env.ALCHEMY_TESTNET_RPC_URL,
+      accounts: [process.env.TESTNET_PRIVATE_KEY],
+    },
+  },
 };
